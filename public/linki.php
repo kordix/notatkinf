e954249a454fc1c9aab1f21a9e6746e4684a9893
Kordian Bober <BOBKOR@ad.bertrand.pl>
2022-02-03 19:52:08 +0100

    linki

---
+++ public/linki.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['zalogowany'])){
+    header('Location: /login.php');
+}
+
+if($_SESSION['zalogowany']){
+    
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notatki</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        #wyloguj{
+            display:block;
+        }
+    </style>
+</head>
+<body>
+
+<div id="app">
+<?php  include 'navbar.php' ?>
+    
+    <div  class="container" id="app">
+
+<linki></linki>
+
+</div>
+
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+    
+
+<?php include('../components/linki.php') ?>
+
+<script src="script.js"></script>
+
+</body>
+</html>
--- public/navbar.php
+++ public/navbar.php
@@ -18,6 +18,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/snippety.php">Snippety</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/linki.php">Linki</a>
+                    </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
